e5c18bf7239657573291e0309e666a5b7bd088f6
arjun kandel <kandelarjun67@gmail.com>
2019-11-22 21:37:58 +0545

    start-up commit

---
+++ css/index.js
@@ -0,0 +1,20 @@
+
+// for menu item
+function expand() {
+    var x = document.getElementById("myMenu");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+//for dropdown item
+function dropexpand() {
+    var x = document.getElementById("myDrop");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
